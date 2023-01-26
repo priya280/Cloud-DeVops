dfd3477160754581ac9a828596370450c7d267a9
Priya Chougale <89589377+priya280@users.noreply.github.com>
2023-01-26 19:26:02 +0530

    Add files via upload

---
+++ 02 - Scripts/01 - Positional parameters/01_pp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh  --------> shell
+# This is script;
+# To print 
+# Author Miss. Priya Chougale
+# Date : 24/01/23
+
+echo "printing number of shell script" $0
+echo "1st parameter" $1
+echo "2nd parameter" $2
+echo " Total number of parameters passed " $#
+echo "All parameters" $*
+
+# echo "All parameters" $@
