6d9404fe3a0c9223a9bd4e917a179cf8ffdc650a
Priya Chougale <89589377+priya280@users.noreply.github.com>
2023-01-28 22:39:25 +0530

    Add files via upload

---
+++ 02 - Scripts/03- Conditional Operators/01_if_condition.sh
@@ -0,0 +1,11 @@
+#!/bin/sh ---> this is given to know which interpreter
+# This shell script is for if condition
+# Author : Priya Chougale
+# Date : 28/ 01/ 23
+
+echo "Enter a number"
+read num
+if [ $num -lt 0 ]
+then
+echo "Number is less than zero"
+fi
